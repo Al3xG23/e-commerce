5fa85ff6a20dc1feb85b1f0c815efaf75a793756
Onyx Vega <mabs.24gl@gmail.com>
2024-05-04 18:57:31 -0400

    added readme

--- models/index.js
+++ models/index.js
@@ -6,24 +6,32 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'category_id'
+  foreignKey: 'category_id',
+  // onDelete: 'cascade',
+  // hooks: true  
 });
 
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id',
+  onDelete: 'cascade',
+  hooks: true
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  foreignKey: 'product_id'
+  foreignKey: 'product_id',
+  // onDelete: 'cascade',
+  // hooks: true
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, { 
   through: ProductTag,
-  foreignKey: 'tag_id'
+  foreignKey: 'tag_id',
+  // onDelete: 'cascade',
+  // hooks: true
 });
 
 module.exports = {
